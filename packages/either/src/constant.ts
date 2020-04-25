0d881de46de5996cd9356857718d4b756c5c86b8
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-04-25 17:17:51 -0300

    :fire: remove unused modules

--- packages/either/src/constant.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Receives a value and returns a new function that returns the value.
- * @param value - Any value.
- */
-export default function constant<T>(value: T) {
-  return () => value;
-}
