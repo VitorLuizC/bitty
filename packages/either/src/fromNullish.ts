fb17db012593b6bc3734ba02d2efe3cdb11d91db
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-05-31 17:53:25 -0300

    :memo: create fromNullish JSDocs + rename NotNullish to NonNullish

--- packages/either/src/fromNullish.ts
+++ packages/either/src/fromNullish.ts
@@ -1,15 +1,20 @@
 import type Either from './Either';
 
-import { isNullish, Nullish, NotNullish } from '@bitty/nullish';
+import { isNullish, Nullish, NonNullish } from '@bitty/nullish';
 
 import Left from './Left.js';
 import Right from './Right.js';
 
 /**
- *
- * @param defaultValue -
+ * Receives a default value and returns a function to create Either instances
+ * from values that can be nullish (`void`, `null` or `undefined`). If so, it
+ * returns a `Left` with received default value, otherwise a `Right` with
+ * non-nullish value.
+ * @param {L} defaultValue - Used as `Left` if value is nullish.
+ * @returns {function(R | null | undefined | void): Either.<L, R>}
+ * @template L, R
  */
 export default function fromNullish<L>(defaultValue: L) {
-  return <R>(value?: Nullish | R): Either<L, NotNullish<R>> =>
-    isNullish(value) ? Left(defaultValue) : Right(value as NotNullish<R>);
+  return <R>(value?: Nullish | R): Either<L, NonNullish<R>> =>
+    isNullish(value) ? Left(defaultValue) : Right(value as NonNullish<R>);
 }
--- packages/nullish/src/main.ts
+++ packages/nullish/src/main.ts
@@ -6,11 +6,11 @@ export type Nullish = void | null | undefined;
 /**
  * Exclude nullish types (`void`, `null` and `undefined`) from T.
  */
-export type NotNullish<T> = T extends Nullish ? never : T;
+export type NonNullish<T> = T extends Nullish ? never : T;
 
 /**
  * Check if value is nullish (`void`, `null` or `undefined`).
- * @param {*} value - Any value.
+ * @param {*} value - Value that will be checked.
  * @returns {Boolean}
  */
 export function isNullish(value: unknown): value is Nullish {
