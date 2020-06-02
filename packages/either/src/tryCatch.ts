3d24115b8d6849e7c0236079c9695b4277dda5f8
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-06-01 23:56:19 -0300

    📝 (either): add JSDoc to tryCatch

--- packages/either/src/tryCatch.ts
+++ packages/either/src/tryCatch.ts
@@ -4,9 +4,12 @@ import Left from './Left.js';
 import Right from './Right.js';
 
 /**
- *
- * @param fn
- * @param onLeft
+ * Receives a function that returns a value used as `Right`, if an error is
+ * thrown it calls `onLeft` with error and returns its result as `Left`.
+ * @param {function(): R} fn
+ * @param {function(*): L} onLeft
+ * @returns {Either.<L, R>}
+ * @template L, R
  */
 export default function tryCatch<L, R>(
   fn: () => R,
