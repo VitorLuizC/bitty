f695123e1994405673bf58050e50532e45fd9483
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-06-05 11:56:31 -0300

    ✅ (either): add unit tests for tryCatch

---
+++ packages/either/src/tryCatch.spec.ts
@@ -0,0 +1,29 @@
+import test from 'ava';
+import tryCatch from './tryCatch.js';
+
+test('tryCatch execute function and use its return as `Right`', (context) => {
+  const value = tryCatch(
+    () => JSON.parse('{"name":"Karl"}'),
+    (error) => error,
+  );
+
+  context.deepEqual(value.unwrap(), { name: 'Karl' });
+  context.true(value.isRight());
+});
+
+test('tryCatch when error is thrown calls onLeft with error and returns its result as `Left`', (context) => {
+  const value = tryCatch(
+    () => JSON.parse('{"name":Karl}'),
+    (error) =>
+      new Error(
+        error instanceof Error
+          ? error.message
+          : typeof error === 'string'
+          ? error
+          : 'Unknown error message.',
+      ),
+  );
+
+  context.true(value.unwrap() instanceof Error);
+  context.true(value.isLeft());
+});
