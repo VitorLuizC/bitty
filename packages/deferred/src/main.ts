8fa5100d42aac6ebe9fe6cc25db99fedd6b3d326
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-01-18 20:38:38 -0300

    ✨ (deferred): add createDeferred function

--- packages/deferred/src/main.ts
+++ packages/deferred/src/main.ts
@@ -1,1 +1,54 @@
-export default function createDeferred() {}
+import type Deferred from './Deferred.js';
+
+export type { Deferred };
+
+type State<T> =
+  | { status: 'PENDING' }
+  | { status: 'RESOLVED'; value: T | PromiseLike<T> }
+  | { status: 'REJECTED'; reason: unknown };
+
+/**
+ * Creates a `Deferred` object that provides a new promise along with methods to
+ * change its state.
+ * @template T
+ * @returns {import('./Deferred.js').default<T>}
+ */
+export default function createDeferred<T>(): Deferred<T> {
+  let state: State<T> = {
+    status: 'PENDING',
+  };
+
+  const deferred: Deferred<T> = {
+    promise: new Promise<T>((resolve, reject) => {
+      switch (state.status) {
+        case 'REJECTED':
+          reject(state.reason);
+          break;
+        case 'RESOLVED':
+          resolve(state.value);
+          break;
+        default:
+          deferred.reject = reject;
+          deferred.resolve = resolve;
+      }
+    }),
+
+    // A placeholder method that fix reject call right after deferred creation.
+    reject(reason) {
+      state = {
+        reason,
+        status: 'REJECTED',
+      };
+    },
+
+    // A placeholder method that fix resolve call right after deferred creation.
+    resolve(value) {
+      state = {
+        value,
+        status: 'RESOLVED',
+      };
+    },
+  };
+
+  return deferred;
+}
