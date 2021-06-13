8fa888431bb54e5449277e3f2253bbf4c8a6b71e
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-06-13 15:43:43 -0300

    ✨ (json): use main.ts to export stuff

--- packages/json/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Json.js';
-export * from './ReadonlyJson.js';
-export { default as fromJSONString } from './fromJSONString.js';
-export { default as toJSONString } from './toJSONString.js';
---
+++ packages/json/src/main.ts
@@ -0,0 +1,4 @@
+export * from './Json.js';
+export * from './ReadonlyJson.js';
+export { default as fromJSON } from './fromJSON.js';
+export { default as toJSON } from './toJSON.js';
