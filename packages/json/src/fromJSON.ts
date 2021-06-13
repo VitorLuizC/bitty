23349ab377f6c8d6876bcb2f0098cfca72d19ca0
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-06-13 20:21:27 -0300

    📝 (json): add example to fromJSON TSDocs

--- packages/json/src/fromJSON.ts
+++ packages/json/src/fromJSON.ts
@@ -1,7 +1,16 @@
 import type { Json } from './Json.js';
 
 /**
- * Transforms a JSON string to {@link Json} value.
+ * Transforms (parses) a JSON string to {@link Json} value.
+ * @example
+ * ```ts
+ * type User = {
+ *   name: string;
+ * };
+ *
+ * const user = fromJSON<User>('{"name":"Carlos Marcos"}');
+ * //=> { name: 'Carlos Marcos' }
+ * ```
  * @param json - The JSON string.
  */
 function fromJSON<T extends Json = Json>(json: string): T {
